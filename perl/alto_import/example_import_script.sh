53e797445c125caf9f9aef508e140a63387c5d10
Mathieu Fannee <mathieu.fannee@inl.nl>
2015-03-25 14:11:05 +0100

    AttestationTool cosmetic change

--- perl/alto_import/example_import_script.sh
+++ perl/alto_import/example_import_script.sh
@@ -3,10 +3,10 @@ USERNAME=username
 PASSWORD=password
 DB=EuropeanaNewspapers
 
-mysql -uimpact -pimpact -himpactdb -e "drop database $DB; create database $DB"
-mysql -uimpact -pimpact -himpactdb  $DB < emptyAttestationDatabase.sql
-mysql -uimpact -pimpact -himpactdb  $DB < types_asis.sql
-mysql -uimpact -pimpact -himpactdb  $DB -e "insert into revisors (name) VALUES (\"jesse\")"
-mysql -uimpact -pimpact -himpactdb  $DB -e "insert into revisors (name) VALUES (\"boukje\")"
-mysql -uimpact -pimpact -himpactdb  $DB -e "insert into revisors (name) VALUES (\"katrien\")"
+mysql -uusername -ppassword -hhostdb -e "drop database $DB; create database $DB"
+mysql -uusername -ppassword -hhostdb  $DB < emptyAttestationDatabase.sql
+mysql -uusername -ppassword -hhostdb  $DB < types_asis.sql
+mysql -uusername -ppassword -hhostdb  $DB -e "insert into revisors (name) VALUES (\"jesse\")"
+mysql -uusername -ppassword -hhostdb  $DB -e "insert into revisors (name) VALUES (\"boukje\")"
+mysql -uusername -ppassword -hhostdb  $DB -e "insert into revisors (name) VALUES (\"katrien\")"
 perl alto2db.pl -h $HOST -u $USERNAME - p $PASSWORD -d $DB [INPUT FILES]
